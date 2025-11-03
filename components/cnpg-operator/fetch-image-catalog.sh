f4a801d939e281e006caac21b1dd5565b5137d0e
Leon Welchert <lichturm@posteo.de>
2025-11-03 22:39:38 +0100

    move cnpg catalog fetch script into subdir

--- components/cnpg-operator/fetch-image-catalog.sh
+++ components/cnpg-operator/fetch-image-catalog.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 curl \
     https://raw.githubusercontent.com/cloudnative-pg/artifacts/refs/heads/main/image-catalogs/catalog-standard-trixie.yaml \
-    -o components/cnpg-operator/catalog-standard-trixie.yaml
+    -o catalog-standard-trixie.yaml
